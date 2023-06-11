b60cee7a2ed30b04e89e1dd298e8b2642420b6a7
mdsojibmiah <mdsojibmiah711@gmail.com>
2023-06-11 19:48:51 +0600

    Parsing- String to number

---
+++ Parsing.dart
@@ -0,0 +1,7 @@
+void main(){
+  /*
+  #The parse() static function allows parsing a string containing numeric literal into a number. The following illustration demonstrates the same −
+   */
+  print(num.parse('12')); 
+  print(num.parse('10.91')); 
+}
