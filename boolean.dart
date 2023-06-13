248167e90509ecc89c357d6b24afab1d6b44e9b0
mdsojibmiah <mdsojibmiah711@gmail.com>
2023-06-13 21:44:37 +0600

    dart boolean done

---
+++ boolean.dart
@@ -0,0 +1,10 @@
+void main(){
+  // Boolean -> true , false
+  /*
+  bool var_name = true;  
+  OR  
+  bool var_name = false  */
+   bool test; 
+   test = 12 > 5; 
+   print("Result -> ${test}");
+}
