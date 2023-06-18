eb8329afd695e4b5413797087b8fa4b5804983bc
mdsojibmiah <mdsojibmiah711@gmail.com>
2023-06-18 21:24:03 +0600

    Dart Interface done

---
+++ dart_interface.dart
@@ -0,0 +1,33 @@
+void main(){
+  // Dart Interface
+  /*
+    #-> Syntax: 
+
+         ->  class Interface_class_name{
+            ...
+          }
+
+          -> class Class_name implements Interface_class_name {
+            ...
+          }
+  
+   */
+   Calculator c = new Calculator(); 
+   print("The gross total : ${c.ret_tot()}"); 
+   print("Discount :${c.ret_dis()}"); 
+
+}
+class Calculate_Total { 
+   int? ret_tot() {} 
+}  
+class Calculate_Discount { 
+   int? ret_dis() {} 
+}
+class Calculator  implements Calculate_Total,Calculate_Discount { 
+   int ret_tot() { 
+      return 1000; 
+   } 
+   int ret_dis() { 
+      return 50; 
+   } 
+}
