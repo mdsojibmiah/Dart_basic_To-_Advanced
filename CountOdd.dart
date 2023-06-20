9e4f51dad9598e6a883a4c035f97192cb3e2ade0
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2023-06-20 18:02:34 +0600

    Create CountOdd number dart.....

---
+++ CountOdd.dart
@@ -0,0 +1,11 @@
+void main() {
+  var num = 0;
+  var count = 0;
+  for (num = 0; num <= 50; num++) {
+    if (num % 2 == 0) {
+      continue;
+    }
+    count++;
+  }
+  print(" The count of odd values between 0 and 50 is: ${count}");
+}
